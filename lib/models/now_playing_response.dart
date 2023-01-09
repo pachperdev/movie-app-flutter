a280f7780826228017a3d96133059856145341e9
pachperdev <pachperdev@gmail.com>
2023-01-09 15:55:17 -0500

    peticion HTTP

---
+++ lib/models/now_playing_response.dart
@@ -0,0 +1,116 @@
+// To parse this JSON data, do
+//
+//     final nowPlayingResponse = nowPlayingResponseFromMap(jsonString);
+
+// ignore_for_file: constant_identifier_names
+
+import 'dart:convert';
+
+class NowPlayingResponse {
+  NowPlayingResponse({
+    this.dates,
+    this.page,
+    this.results,
+    this.totalPages,
+    this.totalResults,
+  });
+
+  Dates? dates;
+  int? page;
+  List<Movie?>? results;
+  int? totalPages;
+  int? totalResults;
+
+  factory NowPlayingResponse.fromJson(String str) =>
+      NowPlayingResponse.fromMap(json.decode(str));
+
+  factory NowPlayingResponse.fromMap(Map<String, dynamic> json) =>
+      NowPlayingResponse(
+        dates: Dates.fromMap(json["dates"]),
+        page: json["page"],
+        results: json["results"] == null
+            ? []
+            : List<Movie?>.from(json["results"]!.map((x) => Movie.fromMap(x))),
+        totalPages: json["total_pages"],
+        totalResults: json["total_results"],
+      );
+}
+
+class Dates {
+  Dates({
+    this.maximum,
+    this.minimum,
+  });
+
+  DateTime? maximum;
+  DateTime? minimum;
+
+  factory Dates.fromJson(String str) => Dates.fromMap(json.decode(str));
+
+  factory Dates.fromMap(Map<String, dynamic> json) => Dates(
+        maximum: DateTime.parse(json["maximum"]),
+        minimum: DateTime.parse(json["minimum"]),
+      );
+
+  Map<String, dynamic> toMap() => {
+        "maximum":
+            "${maximum!.year.toString().padLeft(4, '0')}-${maximum!.month.toString().padLeft(2, '0')}-${maximum!.day.toString().padLeft(2, '0')}",
+        "minimum":
+            "${minimum!.year.toString().padLeft(4, '0')}-${minimum!.month.toString().padLeft(2, '0')}-${minimum!.day.toString().padLeft(2, '0')}",
+      };
+}
+
+class Movie {
+  Movie({
+    this.adult,
+    this.backdropPath,
+    this.genreIds,
+    this.id,
+    this.originalLanguage,
+    this.originalTitle,
+    this.overview,
+    this.popularity,
+    this.posterPath,
+    this.releaseDate,
+    this.title,
+    this.video,
+    this.voteAverage,
+    this.voteCount,
+  });
+
+  bool? adult;
+  String? backdropPath;
+  List<int?>? genreIds;
+  int? id;
+  OriginalLanguage? originalLanguage;
+  String? originalTitle;
+  String? overview;
+  double? popularity;
+  String? posterPath;
+  DateTime? releaseDate;
+  String? title;
+  bool? video;
+  double? voteAverage;
+  int? voteCount;
+
+  factory Movie.fromJson(String str) => Movie.fromMap(json.decode(str));
+
+  factory Movie.fromMap(Map<String, dynamic> json) => Movie(
+        adult: json["adult"],
+        backdropPath: json["backdrop_path"],
+        genreIds: json["genre_ids"] == null
+            ? []
+            : List<int?>.from(json["genre_ids"]!.map((x) => x)),
+        id: json["id"],
+        originalLanguage: originalLanguageValues.map[json["original_language"]],
+        originalTitle: json["original_title"],
+        overview: json["overview"],
+        popularity: json["popularity"].toDouble(),
+        posterPath: json["poster_path"],
+        releaseDate: DateTime.parse(json["release_date"]),
+        title: json["title"],
+        video: json["video"],
+        voteAverage: json["vote_average"].toDouble(),
+        voteCount: json["vote_count"],
+      );
+}
--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 
 class MoviesProvider extends ChangeNotifier {
+  final String _apiKey = 'b113e1523cd29bda4970cfc13c98faf5';
+  final String _baseUrl = 'api.themoviedb.org';
+  final String _language = 'es-ES';
+
   MoviesProvider() {
     // ignore: avoid_print
     print('MoviesProvider inicializado');
@@ -10,5 +16,19 @@ class MoviesProvider extends ChangeNotifier {
   getOnDisplayMovies() async {
     // ignore: avoid_print
     print('getOnDisplayMovies');
+
+    var url = Uri.https(_baseUrl, '/3/movie/now_playing',
+        {'api_key': _apiKey, 'language': _language, 'page': '1'});
+
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      var decodeData =
+          convert.jsonDecode(response.body) as Map<String, dynamic>;
+      // ignore: avoid_print
+      print(decodeData);
+    } else {
+      // ignore: avoid_print
+      print('Request failed with status: ${response.statusCode}.');
+    }
   }
 }
